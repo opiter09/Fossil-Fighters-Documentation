b2f6e565addc2d3bc62ab742a82082aa2c40d123
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-09-22 09:47:53 -0400

    added names

--- FF1/ff1_teamsOutput.py
+++ FF1/ff1_teamsOutput.py
@@ -1,11 +1,27 @@
 import glob
+import os
 newFile = open("Battle Files Teams.txt", "at")
 names = open("ff1_vivoNames.txt", "rt").read()
 
 for file in glob.glob("./battle/bin/**/0.bin", recursive = True):
     data = open(file, "rb")
-    reading = data.read()[148:]
+    full = data.read()
+    nameID = int.from_bytes(full[100:102], "little")
+    reading = full[148:]
     newFile.write("\n" + file[-10:-6] + ":" + "\n")
+
+    newFile.close()
+    newFile = open("Battle Files Teams.txt", "ab")   
+    for root, dirs, files in os.walk("./bin/japanese/textFiles"):
+        for file2 in files:
+            if (int(file2[0:4]) == nameID):
+                nameF = open("./bin/japanese/textFiles/" + file2, "rb")
+                text = "Name: " + nameF.read().decode("UTF-8", errors = "ignore").replace("\0", "\n")
+                newFile.write(text.encode("UTF-8", errors = "ignore"))
+                nameF.close()
+    newFile.close()
+    newFile = open("Battle Files Teams.txt", "at")
+    
     maxi = 0
     for i in [2, 1, 0]:
         vivo = int.from_bytes(reading[(i * 12):(i * 12 + 4)], "little")
