b45dbc96ef2750f5853bc4575a1aa642f20d7b16
opiter09 <92452568+opiter09@users.noreply.github.com>
2023-09-17 22:20:05 -0400

    latest technology

--- FFC/ffc_teamsOutput.py
+++ FFC/ffc_teamsOutput.py
@@ -5,6 +5,7 @@ newFile = open("FFC Teams.txt", "wt")
 newFile.close()
 newFile = open("FFC Teams.txt", "at")
 names = open("ffc_vivoNames.txt", "rt").read()
+enemyNamesF = list(open("ffc_enemyNames.txt", "rt").read().split("\n"))
 
 order = []
 for file in glob.glob("./battle_param/bin/**/0.bin", recursive = True):
@@ -13,24 +14,15 @@ order.sort()
 for file in order:
     data = open("./battle_param/bin/battle_param_defs_" + str(int(file)) + "/0.bin", "rb")
     full = data.read()
-    if (full[71] == 16):
-        shift = 0
-        nameID = full[0x46] - 0x4D
-    else:
-        shift = full[0x6C] + 4
-        nameID = full[0x46 + shift] - 0x4D
+    shift = full[0x38] + 2 - 0x46
+    nameID = full[0x46 + shift] - 0x4D
     reading = full[(112 + shift):]
     newFile.write("\n" + file + ":" + "\n")
 
     newFile.close()
     newFile = open("FFC Teams.txt", "ab")   
-    for root, dirs, files in os.walk("./bin/text_battle_enemy_name/enemyNameFiles"):
-        for file2 in files:
-            if (int(file2[0:2]) == nameID):
-                nameF = open("./bin/text_battle_enemy_name/enemyNameFiles/" + file2, "rb")
-                text = "Name: " + nameF.read().decode("UTF-8", errors = "ignore").replace("\0", "") + " (" + str(nameID).zfill(2) + ")\n"
-                newFile.write(text.encode("UTF-8", errors = "ignore"))
-                nameF.close()
+    text = "Name: " + enemyNamesF[nameID - 1] + " (" + str(nameID).zfill(2) + ")\n"
+    newFile.write(text.encode("UTF-8", errors = "ignore"))
     newFile.close()
     newFile = open("FFC Teams.txt", "at")
     
